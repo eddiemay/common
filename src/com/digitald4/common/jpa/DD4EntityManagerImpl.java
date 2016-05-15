ba914a20aa7138132727eb5c21d7e7fbe56ed339
Eddie Mayfield <eddiemay1999@yahoo.com>
2016-05-15 09:10:53 -0700

    Removal of unused import

--- src/com/digitald4/common/jpa/DD4EntityManagerImpl.java
+++ src/com/digitald4/common/jpa/DD4EntityManagerImpl.java
@@ -3,7 +3,6 @@ package com.digitald4.common.jpa;
 import java.util.List;
 import java.util.Map;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.FlushModeType;
 import javax.persistence.LockModeType;
