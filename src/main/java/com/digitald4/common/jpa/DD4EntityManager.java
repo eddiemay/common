33ae43a8cbe63891317f6c7374f9d79ba1c2c302
eddiemay <eddiemay1999@yahoo.com>
2017-08-25 14:37:45 -0700

    Redeleted JPA, got rid of eclipse project files.

--- src/main/java/com/digitald4/common/jpa/DD4EntityManager.java
+++
@@ -1,9 +0,0 @@
-package com.digitald4.common.jpa;
-
-import java.util.List;
-
-import javax.persistence.EntityManager;
-
-public interface DD4EntityManager extends EntityManager {
-	public <T> List<T> fetchResults(DD4TypedQuery<T> tq) throws Exception;
-}
