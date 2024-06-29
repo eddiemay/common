e8dc6f1df592e1ddcc98731996ce9f3a745567b2
eddiemay <eddiemay1999@yahoo.com>
2024-06-29 08:38:19 -0700

    Setter methods

--- src/main/java/com/digitald4/common/model/Company.java
+++ src/main/java/com/digitald4/common/model/Company.java
@@ -29,51 +29,116 @@ public class Company {
         return website;
     }
 
+    public Company setWebsite(String website) {
+        this.website = website;
+        return this;
+    }
+
     public String getSlogan() {
         return slogan;
+    }
+
+    public Company setSlogan(String slogan) {
+        this.slogan = slogan;
+        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public Company setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
     public String getIpAddress() {
         return ipAddress;
+    }
+
+    public Company setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+        return this;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public Company setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
     public String getPaypal() {
         return paypal;
+    }
+
+    public Company setPaypal(String paypal) {
+        this.paypal = paypal;
+        return this;
     }
 
     public String getStatCounterId() {
         return statCounterId;
     }
 
+    public Company setStatCounterId(String statCounterId) {
+        this.statCounterId = statCounterId;
+        return this;
+    }
+
     public String getStatCounterPart() {
         return statCounterPart;
+    }
+
+    public Company setStatCounterPart(String statCounterPart) {
+        this.statCounterPart = statCounterPart;
+        return this;
     }
 
     public String getContainer() {
         return container;
     }
 
+    public Company setContainer(String container) {
+        this.container = container;
+        return this;
+    }
+
     public String getAddress() {
         return address;
+    }
+
+    public Company setAddress(String address) {
+        this.address = address;
+        return this;
     }
 
     public String getPhone() {
         return phone;
     }
 
+    public Company setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
     public String getFax() {
         return fax;
     }
 
+    public Company setFax(String fax) {
+        this.fax = fax;
+        return this;
+    }
+
     public String getReportFooter() {
         return reportFooter;
+    }
+
+    public Company setReportFooter(String reportFooter) {
+        this.reportFooter = reportFooter;
+        return this;
     }
 }
