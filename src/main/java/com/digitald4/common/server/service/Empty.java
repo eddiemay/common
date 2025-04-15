d4c9004d1674ca56dac461544607c2f999e9d13d
eddiemay <eddiemay1999@yahoo.com>
2025-04-15 08:21:08 -0700

    Made more compatible with GCP.

--- src/main/java/com/digitald4/common/server/service/Empty.java
+++ src/main/java/com/digitald4/common/server/service/Empty.java
@@ -3,9 +3,8 @@ package com.digitald4.common.server.service;
 public class Empty {
   private static final Empty instance = new Empty();
 
-  private Empty() {}
+  public static Empty getInstance() {return instance;}
 
-  public static Empty getInstance() {
-    return instance;
-  }
+  // GCP requires at least 1 property to be serializable.
+  public String getMessage() {return null;}
 }
